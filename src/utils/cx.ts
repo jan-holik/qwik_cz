45ab6949eaa486e2d060fd724ab1b0639f2e41d2
Jan Holík <jh@boziweb.cz>
2024-02-18 17:03:06 +0100

    Refactor cx function and bug fix for arrays in template

--- src/utils/cx.ts
+++ src/utils/cx.ts
@@ -1,24 +1,23 @@
-import clsx, { type ClassValue } from 'clsx';
+import clsx, { type ClassValue } from "clsx";
 
-type TemplateLike = TemplateStringsArray | ClassValue
-type ParamsLike = ClassValue[]
-
+type TemplateLike = TemplateStringsArray | ClassValue;
+type ParamsLike = ClassValue[];
 
 /**
  * Tagged Template Literals for clsx and Tailwind
  * https://al-az.me/blog/clsx-tailwind-template-literal
+ * (Own refactor and bug fix for arrays in template)
  * @param template Template literals Tailwind classes
  * @param params ClassValue from clxs
  * @returns string (Tailwind classes in one row return from clxs)
  */
 const cx = (template: TemplateLike, ...params: ParamsLike): string => {
-  if (typeof template === 'string') return clsx(template, ...params)
+  if (typeof template === "string") return clsx(template, ...params);
 
-  template = Array.isArray(template) ? template : [template]
-  let merged = template.join('') + params.join('')
-  merged = merged.replace(/\s+/g, ' ').trim()
+  const converted = Array.isArray(template) ? template : [template];
+  const merged = converted.join(" ") + params.join(" ");
 
-  return clsx(merged)
-}
+  return clsx(merged.replace(/\s+/g, " ").trim());
+};
 
-export default cx
+export default cx;
